526e587a833c1c3a66307299b77729c174806da1
jdpayne68 <jacques.payne@gmail.com>
2025-01-23 13:26:19 -0600

    Update tgw-saopaulo.tf
    Corrected spelling of "perring" to "Peering"

--- tgw-saopaulo.tf
+++ tgw-saopaulo.tf
@@ -92,7 +92,7 @@ resource "aws_ec2_transit_gateway_route_table_association" "spoke_tgw_vpc_saopau
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.spoke_route_table_saopaulo.id
   provider = aws.sao_paulo
 }
-# Associate Spoke TGW Route Table with New York perring Attachment
+# Associate Spoke TGW Route Table with New York Perring Attachment
 resource "aws_ec2_transit_gateway_route_table_association" "tgw_attachment_association_saopaulo" {
   transit_gateway_attachment_id = aws_ec2_transit_gateway_peering_attachment.hub_to_spoke_saopaulo.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.spoke_route_table_saopaulo.id
